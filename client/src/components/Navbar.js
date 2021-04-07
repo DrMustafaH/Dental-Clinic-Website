e4829933d24124b8ec71f8de0b43ea7422ba93df
DrMustafaH <mustafaharith@gmail.com>
2021-04-07 12:58:24 -0700

    Create navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.scss";
+
+// Navbar component
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div>
+        <img
+          alt="logo"
+          className="main-logo"
+          src="https://st3.depositphotos.com/16835446/19173/v/600/depositphotos_191730704-stock-illustration-tooth-logo-vector-icon-dentist.jpg"
+        ></img>
+      </div>
+    </nav>
+  );
+}
