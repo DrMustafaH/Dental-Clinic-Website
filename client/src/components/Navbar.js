66aee89097759ff485630a9a359358f0471e39fc
DrMustafaH <mustafaharith@gmail.com>
2021-04-09 11:33:16 -0700

    modification is navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,6 +12,10 @@ export default function Navbar() {
           src="https://st3.depositphotos.com/16835446/19173/v/600/depositphotos_191730704-stock-illustration-tooth-logo-vector-icon-dentist.jpg"
         ></img>
       </div>
+      <div>Home</div>
+      <div>About Us</div>
+      <div>Services</div>
+      <div>Book Appointment</div>
     </nav>
   );
 }
